bf60959ee0733ec0bb0c84abb88aa2bfc4da99ce
acaaarr <acarr4632@gmail.com>
2025-08-04 13:09:23 +1200

    Update my_wallpaper.js
    pattern test

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,8 +4,9 @@ let rect_height = 30;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   //pWallpaper.output_mode(GRID_WALLPAPER);
+  //DEVELOP_GLYPH
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
